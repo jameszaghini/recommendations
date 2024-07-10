a921d24620ec61bc0f59260f4a9339514d5ed643
James Zaghini <james@zaghini.com>
2024-07-11 09:06:48 +1000

    use example output

--- lib/models/reccomendation.rb
+++ lib/models/reccomendation.rb
@@ -1,1 +1,1 @@
-Reccomendation = Struct.new(:job_seeker_id, :job_id, :matching_skills_count)
+Reccomendation = Struct.new(:jobseeker_id, :jobseeker_name, :job_id, :job_title, :matching_skill_count, :matching_skill_percent)
